e785b73c6e64ef5d436de29676e69fe36082227b
Shiokiri <xluoying2017@gmail.com>
2025-04-11 23:37:15 +0800

    feat: tilemap

--- world.gd
+++ world.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var camera_2d: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-	var used := tile_map.get_used_rect()
+	var used := tile_map.get_used_rect().grow(-1)
 	var tile_size := tile_map.tile_set.tile_size
 	
 	camera_2d.limit_left = used.position.x * tile_size.x
